2bcaf48d55dc869ec07751d72506dc8554edbdc2
Ram.S <96935904+RamSritharan@users.noreply.github.com>
2024-01-04 18:44:56 -0500

    Adding jest

---
+++ controllers/restaurantCont.test.js
@@ -0,0 +1,11 @@
+const request = require("supertest");
+const app = require("../server");
+
+//get all restaurants
+describe("GET /restaurants", () => {
+  it("it should return all restaurants", async () => {
+    const res = await request(app).get("/restaurants");
+    expect(res.statusCode).toBe(200);
+    done();
+  });
+});
